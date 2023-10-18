b14992a17fef6e7fd376e265712d790843208f4c
oumaima-kboubi <oumaima.kboubi@insat.u-carthage.tn>
2023-10-18 15:15:16 +0100

    initial project + login & logout + remember me + access control + symfony roles

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    // /**
+    //  * Should this authenticator be used for this request?
+    //  *
+    //  * @param Request $request
+    //  * @return boolean|null
+    //  */
+    // public function supports(Request $request): ?bool
+    // {
+    //     return $request->getPathInfo() === '/login' && $request->isMethod('POST');
+    // }
+
+    /**
+     * authetication process
+     *
+     * @param Request $request
+     * @return Passport
+     */
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email', '');
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($request->request->get('password', '')),
+            [
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+                new RememberMeBadge(),
+            ]
+        );
+    }
+
+    /**
+     * What should happen once the user is authenticated?
+     *
+     * @param Request $request
+     * @param TokenInterface $token
+     * @param string $firewallName
+     * @return Response|null
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // Try to redirect the user to their original intended path
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        // If not redirect to home page
+        return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+    }
+
+    /**
+     * On failure
+     *
+     * @param Request $request
+     * @return string
+     */
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
